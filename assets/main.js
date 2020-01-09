a8ae2436bb38fae50744d32a0de58651797939eb
AndrewL <lawna12@gmail.com>
2020-01-09 18:37:11 -0500

    JS added buttons .push functioning

--- assets/main.js
+++ assets/main.js
@@ -1,34 +1,51 @@
+var cityArr = JSON.parse(localStorage.getItem('cityArr')) || [];
+
+
+function renderButtons() {
+    $('#citySearchedBtns').empty()
+
+    for (var i=0; i < cityArr.length; i++) {
+        $('#citySearchedBtns').prepend('<button>' + cityArr[i] + '</button><br/>')
+    };
+}
+renderButtons();
 
 $('#citybtn1').on("click", function(event) {
     event.preventDefault();
-
+   
     var city = $('#cityInput').val();
-    var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&APPID=f7c58c030fde0e2d088010052e6de221"
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&APPID=f7c58c030fde0e2d088010052e6de221"
+    console.log(city)
+    console.log(queryURL)
+    
+    //Ajax call for search button and prepend list
+    cityArr.push(city)
+    console.log(cityArr)
+    localStorage.setItem('cityArr',JSON.stringify(cityArr))
+    renderButtons();
 
     //Ajax call to weatherInfo
     $.ajax({
         url : queryURL,
         method : "GET"
     }).then(function(response) {
-        function addRow(response) {
             var weatherInfo = $('#weatherInfo')
-            var tdCity = $('<h1>')
-            var tdTemp = $('<td>')
-            var tdHumidity = $('<td>')
-            var tdWindspeed = $('<td>')
-            var tdUVIndex = $('<td>')
-            $('.table').children('tbody').append(weatherInfo)
-            weatherInfo.append(tdCity)
-            weatherInfo.append(tdTemp)
-            weatherInfo.append(tdHumidity)
-            weatherInfo.append(tdWindspeed)
-            weatherInfo.append(tdUVIndex)
-            tdCity.text(response.City)
-            tdTemp.text(response.Temperature)
-            tdHumidity.text(response.Humidity)
-            tdWindspeed.text(response.Windspeed)
-            tdUVIndex.text(response.UVIndex)
-        }
+            var cityH1 = $('<li>')
+            var tempLi = $('<li>')
+            var humidityLi = $('<li>')
+            var windspeedLi = $('<li>')
+            var uvIndexLi = $('<li>')
+            $('.ul').children('ul').append(weatherInfo)
+            weatherInfo.append(cityH1)
+            weatherInfo.append(tempLi)
+            weatherInfo.append(humidityLi)
+            weatherInfo.append(windspeedLi)
+            weatherInfo.append(uvIndexLi)
+            cityLi.text(response.City)
+            tempLi.text(response.Temperature)
+            humidityLi.text(response.Humidity)
+            windspeedLi.text(response.Windspeed)
+            uvIndexLi.text(response.UVIndex)
     })
 })
 
