1399bc7405a900666c222f1d86d085e9e5041b53
AndrewL <lawna12@gmail.com>
2020-01-10 02:09:10 -0500

    currentWeather text working!

--- assets/main.js
+++ assets/main.js
@@ -8,44 +8,50 @@ function renderButtons() {
         $('#citySearchedBtns').prepend('<button>' + cityArr[i] + '</button><br/>')
     };
 }
+$('#clearCity').on('click', function() {
+    cityArr = []
+    localStorage.setItem('cityArr',JSON.stringify(cityArr))
+})
+
+
 renderButtons();
 
 $('#citybtn1').on("click", function(event) {
     event.preventDefault();
    
     var city = $('#cityInput').val();
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&APPID=f7c58c030fde0e2d088010052e6de221"
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&units=imperial&APPID=f7c58c030fde0e2d088010052e6de221"
     console.log(city)
     console.log(queryURL)
     
     //Ajax call for search button and prepend list
     cityArr.push(city)
-    console.log(cityArr)
     localStorage.setItem('cityArr',JSON.stringify(cityArr))
     renderButtons();
 
-    //Ajax call to weatherInfo
+    //Ajax call to currentWeather
     $.ajax({
         url : queryURL,
         method : "GET"
     }).then(function(response) {
-            var weatherInfo = $('#weatherInfo')
-            var cityH1 = $('<li>')
-            var tempLi = $('<li>')
-            var humidityLi = $('<li>')
-            var windspeedLi = $('<li>')
-            var uvIndexLi = $('<li>')
-            $('.ul').children('ul').append(weatherInfo)
-            weatherInfo.append(cityH1)
-            weatherInfo.append(tempLi)
-            weatherInfo.append(humidityLi)
-            weatherInfo.append(windspeedLi)
-            weatherInfo.append(uvIndexLi)
-            cityLi.text(response.City)
-            tempLi.text(response.Temperature)
-            humidityLi.text(response.Humidity)
-            windspeedLi.text(response.Windspeed)
-            uvIndexLi.text(response.UVIndex)
+        console.log(response)
+            var currentWeather = $('#currentWeather')
+            var cityH1 = $('<h1>').text(response.name)
+            console.log(response.name)
+            var tempLi = $('<li>').text("Temperature: " + response.main.temp)
+            console.log(response.main.temp)
+            var feels_likeLi = $('<li>').text("Feels like: " + response.main.feels_like)
+            console.log(response.main.feels_like)
+            var humidityLi = $('<li>').text("Humidity: " + response.main.humidity)
+            console.log(response.main.humidity)
+            var windspeedLi = $('<li>').text("Wind Speed: " + response.wind.speed)
+            console.log(response.wind.speed)
+            $('ul').children('ul').append(currentWeather)
+            currentWeather.append(cityH1)
+            currentWeather.append(tempLi)
+            currentWeather.append(feels_likeLi)
+            currentWeather.append(humidityLi)
+            currentWeather.append(windspeedLi)
     })
 })
 
